e6541a46889b6266739df4e613fb35bc20aba8e6
Kai Schnittcher <kai.schnittcher@gmail.com>
2018-08-15 09:10:12 +0200

    Version 1.3 Fix - Überprüfung ob Request Rückgabewert ungleich NULL ist und redundanten Code rausgeworfen

--- IPS-PiHole/module.php
+++ IPS-PiHole/module.php
@@ -7,7 +7,6 @@ class IPS_PiHole extends IPSModule
     {
         //Never delete this line!
         parent::Create();
-        //Connect to Websocket Client
         $this->RegisterPropertyString('Host', '');
         $this->RegisterPropertyInteger('Port', 80);
         $this->RegisterPropertyString('PihToken', '');
@@ -41,9 +40,8 @@ class IPS_PiHole extends IPSModule
         $this->getSummaryRaw();
     }
 
-    private function request($parm)
+    private function request(string $parm)
     {
-
         $url = "http://" . $this->ReadPropertyString("Host") . ":" . $this->ReadPropertyInteger("Port") . "/admin/api.php?" . $parm . "&auth=" . $this->ReadPropertyString("PihToken");
         $this->SendDebug(__FUNCTION__." URL",$url,0);
         $json = @file_get_contents($url);
@@ -60,52 +58,44 @@ class IPS_PiHole extends IPSModule
     public function getStatus()
     {
         $data = $this->request("status");
-        if ($data["status"] == "enabled") {
-            SetValue($this->GetIDForIdent('PihStatus'), true);
-        } else {
-            SetValue($this->GetIDForIdent('PihStatus'), false);
+        if ($data != NULL) {
+            if ($data["status"] == "enabled") {
+                SetValue($this->GetIDForIdent('PihStatus'), true);
+            } else {
+                SetValue($this->GetIDForIdent('PihStatus'), false);
+            }
         }
     }
 
-    public function setStatus($value) {
-        switch ($value) {
-            case true:
-                $data = $this->request("enable");
-                $this->SendDebug(__FUNCTION__,"Pi-hole enabled",0);
-                $this->SendDebug(__FUNCTION__." JSON Result",json_encode($data),0);
-                break;
-            case false:
-                $data = $this->request("disable");
-                $this->SendDebug(__FUNCTION__,"Pi-hole disabled",0);
-                $this->SendDebug(__FUNCTION__." JSON Result",json_encode($data),0);
-                break;
-            default:
-                $this->SendDebug(__FUNCTION__,"Wrong Parameter: $value",0);
-                break;
+    public function setActive(bool $value) {
+        $data = $this->request(($value ? "enable" : "disable"));
+        if ($data != NULL) {
+            switch ($data['status']) {
+                case 'enabled':
+                    SetValue(IPS_GetObjectIDByIdent('PihStatus', $this->InstanceID), true);
+                    break;
+                case 'disabled':
+                    SetValue(IPS_GetObjectIDByIdent('PihStatus', $this->InstanceID), false);
+                    break;
+            }
         }
     }
 
     public function getSummaryRaw() {
         $data = $this->request('summaryRaw');
-        SetValue($this->GetIDForIdent('PihBlockedDomains'), $data['domains_being_blocked']);
-        SetValue($this->GetIDForIdent('PihDNSQueriesToday'), $data['dns_queries_today']);
-        SetValue($this->GetIDForIdent('PihAdsBlockedToday'), $data['ads_blocked_today']);
+        if ($data != NULL) {
+            SetValue($this->GetIDForIdent('PihBlockedDomains'), $data['domains_being_blocked']);
+            SetValue($this->GetIDForIdent('PihDNSQueriesToday'), $data['dns_queries_today']);
+            SetValue($this->GetIDForIdent('PihAdsBlockedToday'), $data['ads_blocked_today']);
+        }
     }
 
     public function RequestAction($Ident, $Value)
     {
         switch ($Ident) {
             case 'PihStatus':
-                if ($Value) {
-                        $this->SendDebug(__FUNCTION__.' Pi-hole Enable', $Value, 0);
-                        $this->setStatus(true);
-                        SetValue(IPS_GetObjectIDByIdent($Ident, $this->InstanceID), true);
-                    } else {
-                    $this->SendDebug(__FUNCTION__.' Pi-hole Disable', $Value, 0);
-                    $this->setStatus(false);
-                    SetValue(IPS_GetObjectIDByIdent($Ident, $this->InstanceID), false);
-                    }
-                    break;
+                $this->setActive($Value);
+                break;
         }
     }
 }
